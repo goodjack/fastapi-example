a70c9a6aee8c4b4491fcc999ded308af5a4de0ec
Jack Lin <goodjack@users.noreply.github.com>
2023-11-10 18:29:40 +0800

    chore: install Sentry

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://e0c03f5bef3eab9b70728375a904fad9@o4506200479170560.ingest.sentry.io/4506200481464321",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 app = FastAPI()
 
 
